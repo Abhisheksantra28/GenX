fd325f9b5d07e15f97436a57f6120097917e290c
Abhishek Santra <abhisheksantra28@gmail.com>
2024-01-07 12:14:58 +0530

    updated Empty component

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -8,9 +8,9 @@ interface EmptyProps {
 
 const Empty = ({label}:EmptyProps) => {
   return (
-    <div className="h-full p-20 flex flex-col items-center justify-center">
+    <div className="h-full  p-20 flex flex-col items-center justify-center">
         <div className="relative h-72 w-72">
-            {/* <Image alt="Empty" src="/empty.png"/> */}
+            <Image alt="Empty" fill src="/empty.png"/>
         </div>
         <p className="text-muted-foreground text-sm text-center">{label}</p>
     </div>
