d6b07bd44fdbb8f947b80353419d54752603ec88
Abhishek Santra <abhisheksantra28@gmail.com>
2024-01-07 12:19:00 +0530

    updated loding component

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,10 +4,10 @@ const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
       <div className="w-10 h-10 relative animate-spin">
-        <Image alt="logo" fill src="" />
+        <Image alt="logo" fill src="/logo.png" />
       </div>
 
-      <p className="text-sm text-muted-foreground">GenX</p>
+      <p className="text-sm text-muted-foreground">GenX is Thinking...</p>
     </div>
   );
 };
