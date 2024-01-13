fa09775712a0c6c3286a8ed712ea55e9a6374586
Abhishek Santra <abhisheksantra28@gmail.com>
2024-01-13 15:40:58 +0530

    complete video generation

---
+++ app/api/video/route.ts
@@ -0,0 +1,45 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN, // ! for not undefined
+});
+
+export const POST = async (req: Request) => {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    // console.log(messages);
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Music prompt is required!!", { status: 400 });
+    }
+
+    const response = await replicate.run(
+      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+      {
+        input: {
+          prompt: prompt
+        }
+      }
+    );
+
+    
+    return NextResponse.json(response, {
+      status: 200,
+    });
+  } catch (error) {
+    console.log("Music generation error", error);
+    return new NextResponse("Error while generating music", {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+};
